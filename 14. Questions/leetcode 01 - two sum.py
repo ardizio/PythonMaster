0db2ab1e3455cba50073a085c21818d74e28fbce
AM <ardizio.manuel@gmail.com>
2023-11-04 09:13:30 +0000

    Nov 4, 2023, 10:13 AM

--- 14. Questions/leetcode 01 - two sum.py
+++ 14. Questions/leetcode 01 - two sum.py
@@ -1,13 +1,5 @@
-"""
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# add two numbers check if target | leetcode 01 | https://leetcode.com/problems/two-sum/
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Question: https://leetcode.com/problems/two-sum/
-
-"""
 #O(n)^2 solution - brute force
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
