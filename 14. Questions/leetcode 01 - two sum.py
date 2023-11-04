49e50d864d5ae46553a0ee10389935326a313988
AM <ardizio.manuel@gmail.com>
2023-11-04 09:27:55 +0000

    Nov 4, 2023, 10:27 AM

--- 14. Questions/leetcode 01 - two sum.py
+++ 14. Questions/leetcode 01 - two sum.py
@@ -1,27 +1,35 @@
 # add two numbers check if target | leetcode 01 | https://leetcode.com/problems/two-sum/
 from typing import List
 
-#O(n)^2 solution - brute force
+# O(n^2) solution - brute force
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Loop through the elements in the nums list using two nested loops.
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
+                # Check if the current pair of elements (nums[i] and nums[j]) sum up to the target.
                 if nums[i] + nums[j] == target:
-                    return [i,j]
+                    # If the condition is met, return the indices of the two numbers that add up to the target.
+                    return [i, j]
+        # If no valid pair is found, return an empty list.
         return []
 
-
-#O(n) solution - hash maps
+# O(n) solution - hash maps
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        
+        prevMap = {}  # Create a dictionary to store previously seen numbers and their indices.
+
         for i, n in enumerate(nums):
-            diff = target-n
+            diff = target - n  # Calculate the difference between the target and the current number.
             if diff in prevMap:
-                return[prevMap[diff],i]
+                # If the difference exists in the dictionary, it means we found a valid pair.
+                # Return the indices of the two numbers that add up to the target.
+                return [prevMap[diff], i]
+            # If the difference is not in the dictionary, add the current number and its index to the dictionary.
             prevMap[n] = i
-        return
+        # If no valid pair is found, return an empty list.
+        return []
+
     
 
 
