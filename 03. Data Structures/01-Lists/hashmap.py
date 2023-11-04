8c177ec40be59bff41c056dc3fb96566683fb8e4
AM <ardizio.manuel@gmail.com>
2023-11-04 17:11:44 +0000

    Nov 4, 2023, 6:11 PM

---
+++ 03. Data Structures/01-Lists/hashmap.py
@@ -0,0 +1,44 @@
+class HashMap:
+    def __init__(self, size=100):
+        self.size = size
+        self.data = [None] * size
+
+    def _hash(self, key):
+        hash_value = 0
+        for char in key:
+            hash_value += ord(char)
+        return hash_value % self.size
+
+    def put(self, key, value):
+        index = self._hash(key)
+        if self.data[index] is None:
+            self.data[index] = []
+        self.data[index].append((key, value))
+
+    def get(self, key):
+        index = self._hash(key)
+        if self.data[index] is not None:
+            for k, v in self.data[index]:
+                if k == key:
+                    return v
+        return None
+
+    def remove(self, key):
+        index = self._hash(key)
+        if self.data[index] is not None:
+            for i, (k, v) in enumerate(self.data[index]):
+                if k == key:
+                    del self.data[index][i]
+                    return
+
+# Example usage of the HashMap class
+my_map = HashMap()
+
+my_map.put("name", "Alice")
+my_map.put("age", 25)
+
+print(my_map.get("name"))  # Output: Alice
+print(my_map.get("age"))   # Output: 25
+
+my_map.remove("name")
+print(my_map.get("name"))  # Output: None
